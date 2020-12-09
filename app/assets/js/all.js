2f92d4fc90a8c68de552530c8ad9c105b7f0a99f
alysachan <alysachansl@gmail.com>
2020-12-09 18:38:50 +0800

    Change navbar to fixed-top

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,15 @@
-console.log('Hello!');
-
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  
+  $(document).on('scroll', () => {
+    if($(document).scrollTop() > 0){
+      $('#navbar').addClass('bg-gray-100')
+      $('#navbar').removeClass('bg-white')
+    }
+
+    else{
+      $('#navbar').addClass('bg-white')
+      $('#navbar').removeClass('bg-gray-100')
+    }
+
+  })
 });
