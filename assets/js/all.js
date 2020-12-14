e8fc0e71ed0126840e4b01abf44e9ee48deb6764
alysachan <alysachansl@gmail.com>
2020-12-14 13:48:56 +0800

    Update 2020-12-14T05:48:54.474Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
     if ($(document).scrollTop() > 0) {
       $('.js-navbar-bg').addClass('bg-gray-100');
       $('.js-navbar-bg').removeClass('bg-white');
+      $('.js-navbar-bg').addClass('fixed-top');
     } else {
       $('.js-navbar-bg').addClass('bg-white');
       $('.js-navbar-bg').removeClass('bg-gray-100');
+      $('.js-navbar-bg').removeClass('fixed-top');
     }
   });
   $('.js-navbar-toggler').on('click', function () {
