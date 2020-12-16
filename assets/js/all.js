d4359e12519a260e97c033c609f3f3e73b773cd7
alysachan <alysachansl@gmail.com>
2020-12-16 15:09:11 +0800

    Update 2020-12-16T07:09:07.098Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,21 +1,40 @@
 "use strict";
 
 $(document).ready(function () {
+  /* Add bottom shadow to navbar when scrolling*/
   $(document).on('scroll', function () {
     if ($(document).scrollTop() > 0) {
-      $('.js-navbar-bg').addClass('bg-gray-100');
-      $('.js-navbar-bg').removeClass('bg-white');
-      $('.js-navbar-bg').addClass('fixed-top');
+      $('.js-navbar').addClass('shadow-sm');
     } else {
-      $('.js-navbar-bg').addClass('bg-white');
-      $('.js-navbar-bg').removeClass('bg-gray-100');
-      $('.js-navbar-bg').removeClass('fixed-top');
+      $('.js-navbar').removeClass('shadow-sm');
     }
   });
+  /* Navbar cancel button in mobile version*/
+
   $('.js-navbar-toggler').on('click', function () {
     $('.js-navbar-toggler__bar-middle').toggle();
     $('.js-navbar-toggler__bar-top').toggleClass('js-navbar-toggler__close--left');
     $('.js-navbar-toggler__bar-bottom').toggleClass('js-navbar-toggler__close--right');
   });
-});
+}); // form validation
+
+(function () {
+  'use strict';
+
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation'); // Loop over them and prevent submission
+
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
 //# sourceMappingURL=all.js.map
